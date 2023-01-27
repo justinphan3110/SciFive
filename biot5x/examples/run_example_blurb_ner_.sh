efdd10ec017ef2e86758e9a87150c87ae6756353
justinphan3110 <justinphan3110@gmail.com>
2023-01-27 14:52:08 -0500

    init example train ner

---
+++ biot5x/examples/run_example_blurb_ner_.sh
@@ -0,0 +1,39 @@
+exit 0;
+
+export model_size=base
+export pretrained_path=biot5x_$model_size
+
+
+############################### NER: BC5CDR_chem ###############################
+task=BC5CDR_chem
+train_file=../data/$task/train_blurb.tsv
+test_file=../data/$task/test_blurb.tsv
+dev_file=../data/$task/dev_blurb.tsv
+gin_file=../configs/biot5x/finetune/base/BC5CDR_chem_blurb.gin
+metric=ner_metric
+
+python3 -u ../src/finetune_biot5x.py \
+  --gin_file=$gin_file \
+  --gin.MODEL_DIR=$model_dir \
+  --gin.INITIAL_CHECKPOINT_PATH=$pretrained_path \
+  --task=$task \
+  --metric=$metric \
+  --train_file=$train_file \
+  --predict_file=$test_file
+
+############################### NER: NCBI_disease ###############################
+task=NCBI_disease
+train_file=../data/$task/train_blurb.tsv
+test_file=../data/$task/test_blurb.tsv
+dev_file=../data/$task/dev_blurb.tsv
+gin_file=../configs/biot5x/finetune/base/NCBI_disease_blurb.gin
+metric=ner_metric
+
+python3 -u ../src/finetune_biot5x.py \
+  --gin_file=$gin_file \
+  --gin.MODEL_DIR=$model_dir \
+  --gin.INITIAL_CHECKPOINT_PATH=$pretrained_path \
+  --task=$task \
+  --metric=$metric \
+  --train_file=$train_file \
+  --predict_file=$test_file
